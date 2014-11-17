4b363d95b421d990db67b62dbc21c5d77c85b353
James Brook <james.brook@spaaza.com>
2014-11-18 00:53:31 +0100

    Bug fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,8 @@ default['beaver']['join_groups'] = []
 default['beaver']['log_dir'] = '/var/log/beaver'
 default['beaver']['pid_dir'] = '/var/run/beaver'
 default['beaver']['pip_package'] = 'beaver==22'
+default['beaver']['logrotate']['options'] = %w(missingok notifempty compress copytruncate)
+default['beaver']['logrotate']['postrotate'] = 'invoke-rc.d logstash_beaver force-reload >/dev/null 2>&1 || true'
 
 default['beaver']['sqs_aws_region'] = 'eu-west-1'
 default['beaver']['sqs_aws_queue'] = 'logstash-input'
